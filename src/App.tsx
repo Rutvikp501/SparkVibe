7edff491a1099b5a21ac082b4b271bb027833dfe
Gainn <it@gainn.co.in>
2023-12-15 17:47:23 +0530

    12-15

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+
+import "./globals.css";
+import { Routes,Route } from "react-router-dom";
+import Home from "./_root/pages/Home"
+import SigninForm from './_auth/forms/SigninForm'
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
+    <main className="flex h-screen">
+   <Routes>
+    {/* public Routes */}
+    <Route path="/sign-in" element={<SigninForm/>} />
+
+    {/* Private Routes */}
+    <Route index  element={<Home/>} />
+   </Routes>
+
+    </main>
   )
 }
 
 export default App
+// color heightlight , tailwind css intellisense 20:41
